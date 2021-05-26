f60e7948b1c90e91b7a191c13c0e2e61c3e6571a
kosorio2 <oso16001@byui.edu>
2021-05-26 15:13:44 -0600

    contact service update

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 
 @Component({
@@ -8,23 +9,26 @@ import { Contact } from '../contact.model';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
-
-  constructor() { }
-
   //SelectedEvent is a custom event that can be used to listen
-  @Output() selectedContactEvent = new EventEmitter<Contact>();
+  // @Output() selectedContactEvent = new EventEmitter<Contact>();
 
-  contacts: Contact[] = [
-    new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg'),
-    new Contact('2', 'Rex Barzee', 'barzeerk@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg')
-  ];
+  contacts: Contact[] = [];
+    // new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg'),
+    // new Contact('2', 'Rex Barzee', 'barzeerk@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg')
 
-  ngOnInit(): void {
+  constructor(private contactService: ContactService) { }
+
+  ngOnInit() {
+    this.contacts = this.contactService.getContacts();
   }
 
   onSelected(contact: Contact) {
-    this.selectedContactEvent.emit(contact);
-    //This needs to be included so that the parent can listen 
+    this.contactService.contactSelectedEvent.emit(contact);
+    //This needs to be included so that the parent can listen
   }
 
 }
+
+
+
+//The constructor runs first, then ngOnInit
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Contact } from '../contacts/contact.model'
+import { Component,OnInit } from '@angular/core';
+import { Contact } from '../contacts/contact.model';
+import { ContactService } from './contact.service'
 
 @Component({
   selector: 'cms-contacts',
@@ -9,9 +10,14 @@ import { Contact } from '../contacts/contact.model'
 export class ContactsComponent implements OnInit {
   selectedContact!: Contact;
 
-  constructor() { }
+  constructor(private contactService: ContactService) {}
 
-  ngOnInit() {
+  ngOnInit(){
+    this.contactService.contactSelectedEvent.subscribe(
+      (contact: Contact) => {
+        this.selectedContact = contact;
+      }
+    )
   }
 
 }
